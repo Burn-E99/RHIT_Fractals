ab5c3c71cf33400d7f164a5ba06a5a89aed422c6
Ean M <ean.milligan@gmail.com>
2015-06-19 09:18:22 -0400

    tsquare

--- simple fractals/tsquare.py
+++ simple fractals/tsquare.py
@@ -1,13 +1,22 @@
 from graphics import *
 from math import *
+from random import *
 import time
 
 
 win = GraphWin("window", 800, 800)
 counter = 0
+Colored = True
+border = "white"
+win.setBackground(border)
+counterMax = 6
 
 def tSquare(ocentx, ocenty, osize, counter, m_dir):
-    if counter == 10:
+    global counterMax
+    global border
+    global Colored
+    
+    if counter == counterMax:
         return
     else:
         counter += 1
@@ -21,13 +30,15 @@ def tSquare(ocentx, ocenty, osize, counter, m_dir):
         np_1 = Point(newcentx  , newcenty )
         np_2 = Point(newcentx - size , newcenty - size)
         nsquare = Rectangle(np_2, np_1)
+        if Colored: nsquare.setFill(color_rgb(random()*255, random()*255, random()*255))
+        nsquare.setOutline(border)
         nsquare.draw(win)
+		
         tSquare(ncentx, ncenty, size, counter,1)
         tSquare(ncentx, ncenty, size, counter,2)
         tSquare(ncentx, ncenty, size, counter,4)
         
     elif m_dir == 2:
-        #
         ncentx = ocentx + osize /2
         ncenty = ocenty - osize / 2
         size = osize / 2
@@ -36,16 +47,15 @@ def tSquare(ocentx, ocenty, osize, counter, m_dir):
         np_1 = Point(newcentx  , newcenty )
         np_2 = Point(newcentx + size , newcenty - size)
         nsquare = Rectangle(np_2, np_1)
+        if Colored: nsquare.setFill(color_rgb(random()*255, random()*255, random()*255))
+        nsquare.setOutline(border)
         nsquare.draw(win)
-        
         
         tSquare(ncentx, ncenty, size, counter,1)
         tSquare(ncentx, ncenty, size, counter,2)
         tSquare(ncentx, ncenty, size, counter,3)
         
     elif m_dir == 3:
-        #
-
         ncentx = ocentx + osize /2
         ncenty = ocenty + osize / 2
         size = osize / 2
@@ -54,13 +64,14 @@ def tSquare(ocentx, ocenty, osize, counter, m_dir):
         np_1 = Point(newcentx  , newcenty )
         np_2 = Point(newcentx + size , newcenty + size)
         nsquare = Rectangle(np_2, np_1)
+        if Colored: nsquare.setFill(color_rgb(random()*255, random()*255, random()*255))
+        nsquare.setOutline(border)
         nsquare.draw(win)
         
         tSquare(ncentx, ncenty, size, counter,4)
         tSquare(ncentx, ncenty, size, counter,2)
         tSquare(ncentx, ncenty, size, counter,3)
     elif m_dir == 4:
-
         ncentx = ocentx - osize /2
         ncenty = ocenty + osize / 2
         size = osize / 2
@@ -69,6 +80,8 @@ def tSquare(ocentx, ocenty, osize, counter, m_dir):
         np_1 = Point(newcentx  , newcenty )
         np_2 = Point(newcentx - size , newcenty + size)
         nsquare = Rectangle(np_2, np_1)
+        if Colored: nsquare.setFill(color_rgb(random()*255, random()*255, random()*255))
+        nsquare.setOutline(border)
         nsquare.draw(win)
         
         tSquare(ncentx, ncenty, size, counter,1)
