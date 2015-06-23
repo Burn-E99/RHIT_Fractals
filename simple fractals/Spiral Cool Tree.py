e13f8c40c1012bacf0666201b34c91c4bcb8ad49
Ean M <ean.milligan@gmail.com>
2015-06-23 14:32:49 -0400

    .

--- simple fractals/Spiral Cool Tree.py
+++ simple fractals/Spiral Cool Tree.py
@@ -2,23 +2,32 @@ from graphics import *
 from math import *
 from time import sleep
 
-win = GraphWin("Cool Tree", 800, 800)
+win = GraphWin("Cool Tree", 1600, 800, autoflush=False)
 win2 = GraphWin("DO NOT CLOSE", 1, 1)
 
-runTimes = 15
+runTimes = 20
 count = 0
-ratio = 3/4
-angle = pi/3
+ratio = 7/8
+angle = pi/6
 breakangle = pi/2
 breakratio = 1/3
+widthratio = (runTimes - 3)/runTimes
+breakwidrat = (runTimes / 2)/runTimes
+width = runTimes
+totCt = 0
 
-def draw_line(sp, ep, ol, na, ct):
+def draw_line(sp, ep, ol, na, ct, wi):
 	global angle
 	global breakangle
 	global ratio
 	global runTimes
+	global widthratio
+	global totCt
+	
+	totCt +=1
 	
 	print(ct)
+	print(totCt)
 	
 	if ct == runTimes:
 		return
@@ -35,16 +44,23 @@ def draw_line(sp, ep, ol, na, ct):
 	n_p2 = Point(dX + ep.getX(), dY + ep.getY())
 	
 	new_line = Line(n_p1, n_p2)
+	new_line.setWidth(wi)
 	new_line.draw(win)
 	
-	draw_line(n_p1, n_p2, nl * ratio, na - angle, ct)
-	draw_line(n_p1, n_p2, nl * breakratio, na + breakangle, ct)
+	if totCt%10000 == 0:
+		win.update()
+		print("updated")
+	
+	
+	draw_line(n_p1, n_p2, nl * ratio, na - angle, ct, wi * widthratio)
+	draw_line(n_p1, n_p2, nl * breakratio, na + breakangle, ct, wi * breakwidrat)
 #
 
 sp_1 = Point(0, 0)
-sp_2 = Point(400, 800)
+sp_2 = Point(800, 800)
 
-draw_line(sp_1, sp_2, 300, -pi/2, count)
+draw_line(sp_1, sp_2, 200, -pi/2, count, width)
 
+win.update()
 win2.getMouse()
 win.close()
